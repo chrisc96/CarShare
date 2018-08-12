6fb4e5a6fe5e524fd899dc0f85ec233b4c5d6a40
chrisc96 <chrisc10196@gmail.com>
2018-08-13 01:41:52 +1200

    got firestore query working and printing

--- CarShare/src/pages/find-a-ride/find-a-ride.ts
+++ CarShare/src/pages/find-a-ride/find-a-ride.ts
@@ -2,13 +2,22 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams, MenuController } from "ionic-angular";
 import { RideListingPage } from "../ride-listing/ride-listing";
 import { NavigationMenuProvider } from "../../providers/navigation-menu/navigation-menu";
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreDocument, AngularFireStoreCollection } from 'angularfire2/firestore';
+import { snapshotChanges } from "../../../node_modules/angularfire2/database";
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+
+
 /**
  * Generated class for the FindARidePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
+interface Listing {
+
+}
 
 @IonicPage()
 @Component({
@@ -17,19 +26,23 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 })
 export class FindARidePage {
 
-  listings;
+  listingsCollection: AngularFireStoreCollection<Listing>
+  listings: Observable<Listing[]> = []
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public menuCtrl: MenuController, 
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public menuCtrl: MenuController,
     public navMenu: NavigationMenuProvider,
-    public database : AngularFireDatabase
+    public afs: AngularFirestore
   ) {
-    this.database.list('listings/').valueChanges().subscribe(
-      data => {
-        this.listings = data
-      }
+    this.listingsCollection = this.afs.collection('listings')
+    // this.listings = 
+    console.log(this.listingsCollection.snapshotChanges().subscribe(e => {
+      e.forEach(elem => {
+        console.log(elem.payload.doc.data());
+      });
+    })
     );
   }
 
